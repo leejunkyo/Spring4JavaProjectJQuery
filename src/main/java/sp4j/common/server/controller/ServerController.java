5318f1d2a56718406bf3b2a047359b738c267aa1
leejunkyo <dlwnsry326@gmail.com>
2020-01-07 23:20:32 +0900

    테이블 자동생성기능추가

--- src/main/java/sp4j/common/server/controller/ServerController.java
+++ src/main/java/sp4j/common/server/controller/ServerController.java
@@ -19,6 +19,7 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -39,11 +40,14 @@ public class ServerController {
 
 	protected Log log = LogFactory.getLog(sessionInterceptor.class);
 	
+	@Transactional
 	@RequestMapping(value = "/server/firstVisit.do")
 	public ModelAndView firstVisit(CommandMap commandMap, HttpServletRequest request) throws Exception {
 		HttpSession session = request.getSession();
 		if(!"N".equals(StringUtil.isNullStr(session.getAttribute("firstVisit")))){
-			log.debug("=====================================              =======================================");
+			log.debug("DB TABLE CREATE SERVER START");
+			log.debug("DB SEVER : "+server);
+			
 			commandMap.put("serverOp", server);
 			
 			List<TableNm> tblList = serverService.tblList(commandMap.getMap());
@@ -53,9 +57,8 @@ public class ServerController {
 	        path = path.substring(0, path.length() - 1);
 	
 	        path = path.substring(0, path.lastIndexOf("/") + 1);
-			String filePath = path+"/dataBaseFile/"+server+"_Create.xlsx";
-			log.debug("DB TABLE CREAT SERVER START");
-			log.debug("DB SEVER : "+server);
+			String filePath = path+"/dataBaseFile/"+server+"/"+server+"_Create.xlsx";
+			
 			InputStream inputStream = new FileInputStream(filePath);
 			Workbook workbook = WorkbookFactory.create(inputStream);
 			Sheet sheet = workbook.getSheetAt(0);
@@ -65,13 +68,17 @@ public class ServerController {
 				Row row = rowItr.next();
 
 				Iterator<Cell> cellItr = row.cellIterator();
-	
+				String chkTable = "";
 				while (cellItr.hasNext()) {
 					boolean flag = false;
 					Cell cell = cellItr.next(); 
 					int index = cell.getColumnIndex();
+					
 					if(index == 0){
-						log.debug("TABLE NAME : "+cell);
+						
+						chkTable = cell.toString();
+						log.debug("==========================================================================================================");
+						log.debug("TABLE NAME : "+chkTable);
 						for(int idx = 0; idx < tblList.size(); idx++){
 							if(cell.toString().equals(tblList.get(idx).getTblNm())){
 								flag = true;
@@ -87,6 +94,29 @@ public class ServerController {
 						log.debug("============================                 DB TABLE CREATE              ============================= ");
 						commandMap.put("query", cell);
 						serverService.exeQuery(commandMap.getMap());
+						
+						if(server.equals("oracle")){
+							log.debug("============================                 ORACLE COMMANT CREATE       ============================= ");
+							filePath = path+"/dataBaseFile/"+server+"/comment/"+chkTable+"_Create.xlsx";
+							inputStream = new FileInputStream(filePath);
+							
+							Workbook workbook2 = WorkbookFactory.create(inputStream);
+							Sheet sheet2 = workbook2.getSheetAt(0);
+							Iterator<Row> rowItr2 = sheet2.iterator();
+							while (rowItr2.hasNext()) {
+								Row row2 = rowItr2.next();
+
+								Iterator<Cell> cellItr2 = row2.cellIterator();
+								while (cellItr2.hasNext()) {
+									Cell cell2 = cellItr2.next(); 
+									
+									commandMap.put("query", cell2);
+									serverService.exeQuery(commandMap.getMap());
+								}
+							}
+						}
+						log.debug("============================                 DB TABLE CREATE  END        ============================= ");
+						
 					}
 				}
 	
@@ -98,7 +128,7 @@ public class ServerController {
 		ModelAndView mv = new ModelAndView("/jlp/JLP10000");
 		return mv;
 	}
-	// �����Ŀ� �°� �� �б�
+	// 셀가져오
     private static Object getValueFromCell(Cell cell) {
         switch(cell.getCellType()) {
             case STRING:
