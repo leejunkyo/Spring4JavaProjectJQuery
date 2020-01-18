6a1600dfb1facaa7a6f56402032c2f303e99d5b8
leejunkyo <dlwnsry326@gmail.com>
2020-01-18 16:02:12 +0900

    상단사이드메뉴스크립

--- src/main/webapp/WEB-INF/jsp/jsm/jsm10110.jsp
+++ src/main/webapp/WEB-INF/jsp/jsm/jsm10110.jsp
@@ -1,10 +1,8 @@
-<script type="text/javascript">
-
-</script>
 <%@ include file="/WEB-INF/jsp/common/ifmHead.jsp" %>
+<script type="text/javascript">
+	Alert("ddd")
+</script>
 <body>
-
-dddd
 <%@ include file="/WEB-INF/jsp/common/ifmFoot.jsp" %>
 
 
--- src/main/webapp/js/common/ifmCommon.js
+++ src/main/webapp/js/common/ifmCommon.js
@@ -1,32 +1,6 @@
-function _ajaxPOST(url,param,successBack){
-	_Ajax(url,'POST',param,successBack);
+function Alert(str){
+	parent.Alert(str)
 }
-
-function _ajaxGET(url,param,successBack){
-	_Ajax(url,'GET',param,successBack);
-}
-
-function _Ajax(url,type,param,successBack){
-	console.log(param);
-	var sData = param;
-	 
-	 $.ajax({
-         url:url,
-         type:type,
-         async: false ,
-         data:sData,
-         dataType:'json',
-         timeout: 3000,
-         contentType : 'application/json; charset=UTF-8',
-         error: function (request, status, error) {      
-        	 
-         }, success: function (data) {
-        	 successBack(data);
-        	 
-         }, beforeSend: function () {
-        	 
-         }
-         
-     });
-	 
+function Confirm(str, head,func) {
+	parent.Confirm(str)
 }
